21e235bab31020f68ca7f03afc31fca6e0741ea0
Anisha-26-pd <anishapd26@gmail.com>
2025-08-26 22:41:46 +0530

    Added first.java program

---
+++ first.java
@@ -0,0 +1,5 @@
+class first{
+ public static void main(String[] args) {
+    System.out.println("I'm trying to connect my all java code to github repository");
+ }
+}
--- hello.java
+++
@@ -1,8 +0,0 @@
-package Javacode;
-
-public class hello {
-static void main(String []args){
-System.out.println("hello Anisha");
-}
-    
-}
