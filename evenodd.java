976bb80a4fc31cd977cc59398a0adb1da460b5ed
Anisha-26-pd <anishapd26@gmail.com>
2025-09-10 22:19:23 +0530

    Added fibonacci.java program

---
+++ evenodd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class evenodd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER A NUMBER");
+       if(sc.hasNextLine()){
+         int num = sc.nextInt();
+         if(num%2==0){
+            System.out.println("Entered number is even");
+         }
+         else
+         System.out.println("Entered number is odd");
+
+       }
+       else 
+       System.out.println("Entered number is invalid please provide whole number");
+    }
+    
+}
