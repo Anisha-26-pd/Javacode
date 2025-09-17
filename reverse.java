390b6089bfb825ca598d330e9c6312548b8c3113
Anisha-26-pd <anishapd26@gmail.com>
2025-09-17 22:15:06 +0530

    Added reverse.java program

---
+++ reverse.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class reverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER THE DIGIT YOU WANT TO REVERSE :");
+        int num =  sc.nextInt();
+        int rev = 0;
+        while(num!=0){
+          int rem =num%10;
+           rev = rev*10 +rem;
+           num = num/10;
+        }
+        System.out.println("YOUR REVERSE NUMBER :" + rev + " " +num);
+    }
+}
